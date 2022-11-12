2a95fcfd18c29969357fa79ec6d54487b4b0a5af
Kunjesh Kumar <kunjesh.kumar@qure.ai>
2022-11-12 16:37:20 +0530

    revamped portfolio site

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+  pageExtensions: ['jsx', 'mdx'],
+  reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
+}
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig)
